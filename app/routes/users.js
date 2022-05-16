40bd282027e8d0ffca45c8b29c63791b7c05a85a
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-05-16 12:49:53 -0300

    using NeDB

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,12 @@
-
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename:'users.db',
+    autoload: 'true.'
+})
 module.exports  = (app) => {
 
 
 app.get('/users', (req, res) => {
-
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json')
     res.json({
@@ -16,12 +19,22 @@ app.get('/users', (req, res) => {
 
 });
 
-app.get('/users/admin', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json')
-    res.json({
-       users:[]
-    });
+app.post('/users', (req, res) => {
+
+    db.insert(req.body, (err, user) =>{
+
+        if (err ) {
+            console.log('error: ${err}');
+            res.status(400).json({
+                error: err
+
+            });
+        }
+        else {
+
+            res.status(200).json(user);
+        }
+    })
 })
 
 
